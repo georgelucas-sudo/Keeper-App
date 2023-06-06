a63dcf443dec2e312bc99103518f3efcea89a41c
Yiga George Lucas <georgedalucas111@gmail.com>
2023-06-06 13:12:45 +0300

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function Footer() {
+    const currentYear = new Date().getFullYear();
+    return <footer>
+        <p>CopyRight © {currentYear}</p>
+    </footer>
+}
+export default Footer;
